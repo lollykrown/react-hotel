c486d7a9d3df17899e5c1cf0019cf604a14b4315
Kay <joe_kayu@yahoo.com>
2020-10-03 00:09:34 +0100

    .

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
+      <>
+      <Navbar />
       <Switch>
         <Route exact path ='/' component={Home}/>
         <Route exact path ='/rooms' component={Rooms}/>
         <Route exact path ='/rooms/:id' component={SingleRoom}/>
         <Route component={Error} />
       </Switch>
-    </div>
+      </>
   );
 }
 
